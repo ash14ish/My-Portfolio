9ae28b2c33823e57cc15b43bccc5f5871368b5e9
ash14ish <ash14ishh@gmail.com>
2022-06-25 04:13:21 +0530

    portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
 import React from "react";
 
+import Header from "./components/Home/Home";
+import Navbar from "./components/Navbar/Navbar";
+import About from "./components/about/about";
+import Skills from "./components/Skills/Skills";
+import SkillsDetails from "./components/SkillsDetails/SkillsDetails";
+import Projects from "./components/Projects/Project";
+import Contact from "./components/Contact/Contact";
+import Footer from "./components/Footer/Footer";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <Header />
+      <Navbar />
+      <About />
+      <Skills />
+      <SkillsDetails />
+      <Projects />
+      <Contact />
+      <Footer />
+    </>
+  );
 };
 
 export default App;
