0935e862a3ca21cd6efb3e5f1122003fb97163b9
ash14ish <ash14ishh@gmail.com>
2022-06-25 14:36:19 +0530

    portfolio

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
 import { AiOutlineHome, AiOutlineUser } from "react-icons/ai";
 import { VscTools } from "react-icons/vsc";
@@ -6,25 +6,51 @@ import { MdWorkOutline } from "react-icons/md";
 import { BsChatDots } from "react-icons/bs";
 
 const Navbar = () => {
+  const [activeNav, setActiveNav] = useState("#");
+
+  const activeNavToggleHandler = id => {
+    setActiveNav(id);
+  };
+
   return (
     <nav>
-      <a href="#" className="active">
+      <a
+        href="#"
+        onClick={activeNavToggleHandler.bind(null, "home")}
+        className={activeNav === "home" ? "active" : ""}
+      >
         <AiOutlineHome />
       </a>
 
-      <a href="#about">
+      <a
+        href="#about"
+        onClick={activeNavToggleHandler.bind(null, "about")}
+        className={activeNav === "about" ? "active" : ""}
+      >
         <AiOutlineUser />
       </a>
 
-      <a href="#skills">
+      <a
+        href="#skills"
+        onClick={activeNavToggleHandler.bind(null, "skills")}
+        className={activeNav === "skills" ? "active" : ""}
+      >
         <VscTools />
       </a>
 
-      <a href="#projects">
+      <a
+        href="#projects"
+        onClick={activeNavToggleHandler.bind(null, "projects")}
+        className={activeNav === "projects" ? "active" : ""}
+      >
         <MdWorkOutline />
       </a>
 
-      <a href="#contact">
+      <a
+        href="#contact"
+        onClick={activeNavToggleHandler.bind(null, "contact")}
+        className={activeNav === "contact" ? "active" : ""}
+      >
         <BsChatDots />
       </a>
     </nav>
