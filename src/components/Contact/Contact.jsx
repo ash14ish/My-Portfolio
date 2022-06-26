408da194c87c93e66010b41639ae5022c9ac2b83
ash14ish <ash14ishh@gmail.com>
2022-06-27 04:16:36 +0530

    portfolio

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Contact.css";
-import { SiGmail } from "react-icons/si";
+import { SiGmail, SiMessenger, SiSkype } from "react-icons/si";
 
 const Contact = () => {
   return (
@@ -14,7 +14,33 @@ const Contact = () => {
             <SiGmail />
             <h4>Gmail</h4>
             <h5>ashish7347@gmail.com</h5>
-            <a href="mailto:ashish7347@gmail.com">Send a mail</a>
+            <a href="mailto:ashish7347@gmail.com">Send an e-mail</a>
+          </article>
+
+          <article className="contact__option">
+            <SiMessenger />
+            <h4>Messenger</h4>
+            <h5>ashish141994</h5>
+            <a
+              href="https://m.me/ash14ish/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Send a message
+            </a>
+          </article>
+
+          <article className="contact__option">
+            <SiSkype />
+            <h4>Skype</h4>
+            <h5>ashish7347</h5>
+            <a
+              href="https://join.skype.com/invite/yApaeI1vgreD"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Skype me
+            </a>
           </article>
         </div>
       </div>
