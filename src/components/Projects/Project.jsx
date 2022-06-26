9a14ae27c876c5a482be1f730b472eac7f16ab8b
ash14ish <ash14ishh@gmail.com>
2022-06-26 11:17:06 +0530

    portfolio

--- src/components/Projects/Project.jsx
+++ src/components/Projects/Project.jsx
@@ -2,7 +2,14 @@ import React from "react";
 import "./Projects.css";
 
 const Projects = () => {
-  return <section id="projects">Projects</section>;
+  return (
+    <section id="projects">
+      <h5>Fully Responsive</h5>
+      <h2>Projects</h2>
+
+      <div className="container portfolio__container"></div>
+    </section>
+  );
 };
 
 export default Projects;
