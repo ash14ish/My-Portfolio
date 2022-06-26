904dd51d36b130c48595db5f576621eb866c2142
ash14ish <ash14ishh@gmail.com>
2022-06-26 15:58:07 +0530

    portfolio

--- src/components/Projects/Project.jsx
+++ src/components/Projects/Project.jsx
@@ -1,17 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Projects.css";
+import projectsData from "./projects-data.js";
+import { SiNetlify, SiGithub } from "react-icons/si";
 
 import Img1 from "../../assets/project-1.png";
-import Img2 from "../../assets/project-2.png";
-import Img3 from "../../assets/project-3.png";
-import Img4 from "../../assets/project-4.png";
-import Img5 from "../../assets/project-5.png";
-import Img6 from "../../assets/project-6.png";
-import Img7 from "../../assets/project-7.png";
-import Img8 from "../../assets/project-8.png";
-import Img9 from "../../assets/project-9.png";
-import Img10 from "../../assets/project-10.png";
-import Img11 from "../../assets/project-11.png";
 
 const Projects = () => {
   return (
@@ -20,203 +12,27 @@ const Projects = () => {
       <h2>Projects</h2>
 
       <div className="container projects__container">
-        <article className="project">
-          <div className="project__image">
-            <img src={Img1} alt="guess-the-number"></img>
-          </div>
+        {projectsData.map(item => {
+          return (
+            <article key={item.id} className="project">
+              <div className="project__image">
+                <img src={item.image} alt={item.title}></img>
+              </div>
 
-          <h3>Guess The Number</h3>
+              <h3>{item.title}</h3>
 
-          <div className="project__cta">
-            <a href="" target="_blank" className="btn">
-              Code
-            </a>
+              <div className="project__cta">
+                <a href={item.github} target="_blank" className="btn">
+                  <SiGithub className="project__cta-icon" /> Code
+                </a>
 
-            <a href="" target="_blank" className="btn btn-primary">
-              Demo
-            </a>
-          </div>
-        </article>
-
-        <article className="project">
-          <div className="project__image">
-            <img src={Img1} alt="guess-the-number"></img>
-          </div>
-
-          <h3>Guess The Number</h3>
-
-          <div className="project__cta">
-            <a href="" target="_blank" className="btn">
-              Code
-            </a>
-
-            <a href="" target="_blank" className="btn btn-primary">
-              Demo
-            </a>
-          </div>
-        </article>
-
-        <article className="project">
-          <div className="project__image">
-            <img src={Img1} alt="guess-the-number"></img>
-          </div>
-
-          <h3>Guess The Number</h3>
-
-          <div className="project__cta">
-            <a href="" target="_blank" className="btn">
-              Code
-            </a>
-
-            <a href="" target="_blank" className="btn btn-primary">
-              Demo
-            </a>
-          </div>
-        </article>
-
-        <article className="project">
-          <div className="project__image">
-            <img src={Img1} alt="guess-the-number"></img>
-          </div>
-
-          <h3>Guess The Number</h3>
-
-          <div className="project__cta">
-            <a href="" target="_blank" className="btn">
-              Code
-            </a>
-
-            <a href="" target="_blank" className="btn btn-primary">
-              Demo
-            </a>
-          </div>
-        </article>
-
-        <article className="project">
-          <div className="project__image">
-            <img src={Img1} alt="guess-the-number"></img>
-          </div>
-
-          <h3>Guess The Number</h3>
-
-          <div className="project__cta">
-            <a href="" target="_blank" className="btn">
-              Code
-            </a>
-
-            <a href="" target="_blank" className="btn btn-primary">
-              Demo
-            </a>
-          </div>
-        </article>
-
-        <article className="project">
-          <div className="project__image">
-            <img src={Img1} alt="guess-the-number"></img>
-          </div>
-
-          <h3>Guess The Number</h3>
-
-          <div className="project__cta">
-            <a href="" target="_blank" className="btn">
-              Code
-            </a>
-
-            <a href="" target="_blank" className="btn btn-primary">
-              Demo
-            </a>
-          </div>
-        </article>
-
-        <article className="project">
-          <div className="project__image">
-            <img src={Img1} alt="guess-the-number"></img>
-          </div>
-
-          <h3>Guess The Number</h3>
-
-          <div className="project__cta">
-            <a href="" target="_blank" className="btn">
-              Code
-            </a>
-
-            <a href="" target="_blank" className="btn btn-primary">
-              Demo
-            </a>
-          </div>
-        </article>
-
-        <article className="project">
-          <div className="project__image">
-            <img src={Img1} alt="guess-the-number"></img>
-          </div>
-
-          <h3>Guess The Number</h3>
-
-          <div className="project__cta">
-            <a href="" target="_blank" className="btn">
-              Code
-            </a>
-
-            <a href="" target="_blank" className="btn btn-primary">
-              Demo
-            </a>
-          </div>
-        </article>
-
-        <article className="project">
-          <div className="project__image">
-            <img src={Img1} alt="guess-the-number"></img>
-          </div>
-
-          <h3>Guess The Number</h3>
-
-          <div className="project__cta">
-            <a href="" target="_blank" className="btn">
-              Code
-            </a>
-
-            <a href="" target="_blank" className="btn btn-primary">
-              Demo
-            </a>
-          </div>
-        </article>
-
-        <article className="project">
-          <div className="project__image">
-            <img src={Img1} alt="guess-the-number"></img>
-          </div>
-
-          <h3>Guess The Number</h3>
-
-          <div className="project__cta">
-            <a href="" target="_blank" className="btn">
-              Code
-            </a>
-
-            <a href="" target="_blank" className="btn btn-primary">
-              Demo
-            </a>
-          </div>
-        </article>
-
-        <article className="project">
-          <div className="project__image">
-            <img src={Img1} alt="guess-the-number"></img>
-          </div>
-
-          <h3>Guess The Number</h3>
-
-          <div className="project__cta">
-            <a href="" target="_blank" className="btn">
-              Code
-            </a>
-
-            <a href="" target="_blank" className="btn btn-primary">
-              Demo
-            </a>
-          </div>
-        </article>
+                <a href={item.demo} target="_blank" className="btn btn-primary">
+                  <SiNetlify className="project__cta-icon" /> Demo
+                </a>
+              </div>
+            </article>
+          );
+        })}
       </div>
     </section>
   );
---
+++ src/components/Projects/projects-data.js
@@ -0,0 +1,103 @@
+import Img1 from "../../assets/project-1.png";
+import Img2 from "../../assets/project-2.png";
+import Img3 from "../../assets/project-3.png";
+import Img4 from "../../assets/project-4.png";
+import Img5 from "../../assets/project-5.png";
+import Img6 from "../../assets/project-6.png";
+import Img7 from "../../assets/project-7.png";
+import Img8 from "../../assets/project-8.png";
+import Img9 from "../../assets/project-9.png";
+import Img10 from "../../assets/project-10.png";
+import Img11 from "../../assets/project-11.png";
+
+const projectsData = [
+  {
+    id: 1,
+    image: Img1,
+    title: "Guess The Number",
+    github: "https://github.com/ash14ish/01-guess-the-number",
+    demo: "https://app.netlify.com/sites/guess-it-win-it/overview",
+  },
+
+  {
+    id: 2,
+    image: Img2,
+    title: "Pig Game",
+    github: "https://github.com/ash14ish/02-pig-game",
+    demo: "https://epig-game.netlify.app/",
+  },
+
+  {
+    id: 3,
+    image: Img3,
+    title: "Bank User Login",
+    github: "https://github.com/ash14ish/03-bankist-user-login",
+    demo: "https://bank-user-login.netlify.app/",
+  },
+
+  {
+    id: 4,
+    image: Img4,
+    title: "Bankist Website",
+    github: "https://github.com/ash14ish/04-bankist-website",
+    demo: "https://the-best-bank.netlify.app/",
+  },
+
+  {
+    id: 5,
+    image: Img5,
+    title: "Mapish",
+    github: "https://github.com/ash14ish/05-mapish",
+    demo: "https://mapish.netlify.app/",
+  },
+
+  {
+    id: 6,
+    image: Img6,
+    title: "Forkifite",
+    github: "https://github.com/ash14ish/06-forkify",
+    demo: "https://forkifite.netlify.app/",
+  },
+
+  {
+    id: 7,
+    image: Img7,
+    title: "Photowall",
+    github: "https://github.com/ash14ish/07-photowall",
+    demo: "https://the-photo-wall.netlify.app/",
+  },
+
+  {
+    id: 8,
+    image: Img8,
+    title: "Expense Tracker",
+    github: "https://github.com/ash14ish/08-expense-tracker",
+    demo: "https://x-pens-tracker.netlify.app/",
+  },
+
+  {
+    id: 9,
+    image: Img9,
+    title: "Food Order App",
+    github: "https://github.com/ash14ish/09-food-order-app",
+    demo: "https://f00d-0rder-app.netlify.app/",
+  },
+
+  {
+    id: 10,
+    image: Img10,
+    title: "Up-Quote-Load",
+    github: "https://github.com/ash14ish/10-up-quote-load",
+    demo: "https://up-quote-load.netlify.app/",
+  },
+
+  {
+    id: 11,
+    image: Img11,
+    title: "My Portfolio",
+    github: "https://github.com/ash14ish/My-Portfolio",
+    demo: "https://am-responsive-portfolio.netlify.app/",
+  },
+];
+
+export default projectsData;
