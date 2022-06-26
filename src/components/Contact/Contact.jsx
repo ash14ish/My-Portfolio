ba869c76ad8af5124fe55b079d1cbf4176fdda14
ash14ish <ash14ishh@gmail.com>
2022-06-27 03:48:24 +0530

    portfolio

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,8 +1,25 @@
 import React from "react";
 import "./Contact.css";
+import { SiGmail } from "react-icons/si";
 
 const Contact = () => {
-  return <section id="contact">Contact</section>;
+  return (
+    <section id="contact">
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <SiGmail />
+            <h4>Gmail</h4>
+            <h5>ashish7347@gmail.com</h5>
+            <a href="mailto:ashish7347@gmail.com">Send a mail</a>
+          </article>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Contact;
